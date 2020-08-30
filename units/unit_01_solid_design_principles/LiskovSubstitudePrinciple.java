3a6cf040a383b6721bf371489d6cbf0efdcf1bd7
Alexander Tilkin <alextilk@gmail.com>
2020-08-30 19:15:38 +0300

    I added the Liskov Substitude Principle

---
+++ units/unit_01_solid_design_principles/LiskovSubstitudePrinciple.java
@@ -0,0 +1,79 @@
+package unit_01_solid_design_principles;
+
+
+class Rectangle{
+	private int width;
+	private int height;
+	
+	public Rectangle() {
+		
+	}
+	
+	public Rectangle(int width, int height) {
+		this.width = width;
+		this.height = height;
+	}
+
+	public int getWidth() {
+		return width;
+	}
+	
+	public void setWidth(int width) {
+		this.width = width;
+	}
+	
+	public int getHeight() {
+		return height;
+	}
+	
+	public void setHeight(int height) {
+		this.height = height;
+	}
+	
+	public int getArea() {
+		return width * height;
+	}
+	
+	@Override
+	public String toString() {
+		return "Width: " + width + "; height " + height;
+	}
+}
+
+class Square extends Rectangle{
+	public Square() {
+	}
+	
+	public Square(int side) {
+		super(side, side);
+	}
+	
+	public void setWidth(int width) {
+		super.setHeight(width);
+		super.setWidth(width);
+	}
+	
+	public void setHeight(int height) {
+		super.setHeight(height);
+		super.setWidth(height);
+	}
+}
+
+public class LiskovSubstitudePrinciple {
+	  static void test(Rectangle rectangle)
+	  {
+	    int width = rectangle.getWidth();
+	    rectangle.setHeight(10);
+	    System.out.println("The Expected area size is " + (width * 10) + ", actual are size is " + rectangle.getArea());
+	  }
+
+	  public static void main(String[] args) {
+	    Rectangle rectangle = new Rectangle(2, 3);
+	    test(rectangle);
+
+	    Rectangle square = new Square();
+	    square.setHeight(5);
+	    square.setWidth(10);
+	    test(square);
+	  }
+}
