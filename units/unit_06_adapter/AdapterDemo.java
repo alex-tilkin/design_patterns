dff1da30a7197628ab88770174cd02fd0fdfac23
Alexander Tilkin <alextilk@gmail.com>
2020-10-05 19:59:40 +0300

    I added the Adapter unit

---
+++ units/unit_06_adapter/AdapterDemo.java
@@ -0,0 +1,76 @@
+package unit_06_adapter;
+
+interface IShape {
+    void draw(int x, int y, int z, int j);
+}
+
+class Line {
+    public void draw(int x1, int y1, int x2, int y2) {
+        System.out.println("Line from point A(" + x1 + ";" + y1 + "), to point B(" + x2 + ";" + y2 + ")");
+    }
+}
+
+class Quadrate {
+    public void draw(int x, int y, int width, int height) {
+        System.out.println("Rectangle with coordinate left-down point (" + x + ";" + y + "), width: " + width
+                + ", height: " + height);
+    }
+}
+
+class LineAdapter implements IShape {
+    private Line adaptee;
+
+    public LineAdapter(Line line) {
+        this.adaptee = line;
+    }
+
+    @Override
+    public void draw(int x1, int y1, int x2, int y2) {
+        adaptee.draw(x1, y1, x2, y2);
+    }
+}
+
+class RectangleAdapter implements IShape {
+    private Quadrate adaptee;
+
+    public RectangleAdapter(Quadrate rectangle) {
+        this.adaptee = rectangle;
+    }
+
+    @Override
+    public void draw(int x1, int y1, int x2, int y2) {
+        int x = Math.min(x1, x2);
+        int y = Math.min(y1, y2);
+        int width = Math.abs(x2 - x1);
+        int height = Math.abs(y2 - y1);
+        adaptee.draw(x, y, width, height);
+    }
+}
+
+/*public class AdapterDemo {
+    public static void main(String[] args) {
+        Object[] shapes = {new Line(), new Rectangle()};
+        int x1 = 10, y1 = 20;
+        int x2 = 30, y2 = 60;
+        int width = 40, height = 40;
+        for (Object shape : shapes) {
+            if (shape.getClass().getSimpleName().equals("Line")) {
+                ((Line)shape).draw(x1, y1, x2, y2);
+            } else if (shape.getClass().getSimpleName().equals("Rectangle")) {
+                ((Rectangle)shape).draw(x2, y2, width, height);
+            }
+        }
+    }
+}*/
+
+public class AdapterDemo {
+    public static void main(String[] args) {
+        IShape[] shapes = {new RectangleAdapter(new Quadrate()), new LineAdapter(new Line())};
+        int x1 = 10, y1 = 20;
+        int x2 = 30, y2 = 60;
+        
+        for (IShape shape : shapes) {
+            shape.draw(x1, y1, x2, y2);
+        }
+    }
+}
---
+++ units/unit_06_adapter/AdapterExercise.java
@@ -0,0 +1,46 @@
+package unit_06_adapter;
+
+class Square {
+	public int side;
+
+	public Square(int side) {
+		this.side = side;
+	}
+}
+
+interface Rectangle {
+	int getWidth();
+
+	int getHeight();
+
+	default int getArea() {
+		return getWidth() * getHeight();
+	}
+}
+
+class SquareToRectangleAdapter implements Rectangle {
+	public SquareToRectangleAdapter(Square square) {
+		// todo
+	}
+
+	@Override
+	public int getWidth() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getHeight() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+}
+
+public class AdapterExercise {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
