ed939fada5c4fa1e12ed9d1f95bf94dc5113ba91
Alexander Tilkin <alextilk@gmail.com>
2021-01-23 21:42:46 +0200

    I added Memento unit

---
+++ units/unit_18_memento/MementoExample.java
@@ -0,0 +1,54 @@
+package unit_18_memento;
+
+
+class Memento{
+	private int balance;
+	
+	public Memento(int balance) {
+		this.balance = balance;
+	}
+
+	public int getBalance() {
+		return balance;
+	}
+}
+
+class BankAccount{
+	private int balance;
+	
+	public BankAccount(int balance) {
+		this.balance = balance;
+	}
+	
+	public Memento deposit(int amount) {
+		this.balance += amount;
+		
+		return new Memento(this.balance);
+	}
+	
+	public void restore(Memento memento) {
+		balance = memento.getBalance();
+	}
+	
+	@Override
+	public String toString() {
+		return String.format("The balance is %d", balance);
+	}
+}
+
+public class MementoExample {
+
+	public static void main(String[] args) {
+		BankAccount bankAccount = new BankAccount(400);
+		
+		Memento memento1 = bankAccount.deposit(10);
+		Memento memento2 = bankAccount.deposit(30);
+		System.out.println(bankAccount);
+		
+		bankAccount.restore(memento2);
+		System.out.println(bankAccount);
+		
+		bankAccount.restore(memento1);
+		System.out.println(bankAccount);
+	}
+}
---
+++ units/unit_18_memento/MementoExercise.java
@@ -0,0 +1,40 @@
+package unit_18_memento;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Token {
+	public int value = 0;
+
+	public Token(int value) {
+		this.value = value;
+	}
+}
+
+class MementoEx {
+	public List<Token> tokens = new ArrayList<>();
+}
+
+class TokenMachine {
+	public List<Token> tokens = new ArrayList<>();
+
+	public MementoEx addToken(int value) {
+		// todo
+	}
+
+	public MementoEx addToken(Token token) {
+		// todo
+	}
+
+	public void revert(Memento m) {
+		// todo
+	}
+}
+
+public class MementoExercise {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+}
