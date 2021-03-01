500d704903366a05413204603defd426ac1f0b7c
Alexander Tilkin <alextilk@gmail.com>
2021-03-01 15:53:19 +0200

    builder exercise

---
+++ units/unit_02_builder/BuilderExercise.java
@@ -0,0 +1,24 @@
+package unit_02_builder;
+
+class CodeBuilder {
+	public CodeBuilder(String className) {
+		// todo
+	}
+
+	public CodeBuilder addField(String name, String type) {
+		// todo
+		return null;
+	}
+
+	@Override
+	public String toString() {
+		// todo
+		return null;
+	}
+}
+
+public class BuilderExercise {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	}
+}
